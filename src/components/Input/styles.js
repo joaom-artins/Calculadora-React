7a3ae8ab6dbd02ccc46d87f00e3764ae8102aa73
Joao Martins <milanezijoao51@gmail.com>
2024-08-13 14:32:04 -0300

    commit do projeto

---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width:100%;
+    height: 75px;
+    background-color: #aaffaa;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size:24px;
+    font-family:' Roboto';
+
+    input{
+        width:100%;
+        height: 75px;
+        background-color: #aaffaa;
+        border:0;
+        padding : 0 10px;
+        float: right;
+        clear:both;
+        font-size:24px;
+        font-family:' Roboto';
+    }
+`
